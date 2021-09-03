11a02be4397ce8376d8ae05c4aa692cde5926162
YunXinZhiCheng <867395636@qq.com>
2021-09-03 16:58:48 +0800

    axios封装请求模块

---
+++ src/utils/request.js
@@ -0,0 +1,13 @@
+// 导入axios
+import axios from 'axios'
+
+// 创建axios实例
+const request = axios.create({
+  baseURL: 'http://localhost:3005',
+
+    // 请求拦截器
+    // 响应拦截器
+})
+
+// 导出axios实例
+export default request
