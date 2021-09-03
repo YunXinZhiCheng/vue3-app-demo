135a76d4c740a3e714655f675d8acfe5c286037f
YunXinZhiCheng <867395636@qq.com>
2021-09-03 19:03:54 +0800

    实现基本登录功能

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 // 创建axios实例
 const request = axios.create({
-  baseURL: 'http://localhost:3005',
+  baseURL: 'http://localhost:3005/',
 
     // 请求拦截器
     // 响应拦截器
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,28 +6,69 @@
     <van-form @submit="onSubmit">
       <!-- 登录密码 -->
       <van-cell-group inset>
+        <!-- 双向数据绑定：v-model -->
         <van-field
           name="userName"
           placeholder="请输入用户名"
           left-icon="manager"
+          v-model="userName"
         />
         <van-field
           type="password"
           name="userPwd"
           placeholder="请输入密码"
           left-icon="lock"
+          v-model="userPwd"
         />
       </van-cell-group>
-      <!-- 提交按钮 -->
+      <!-- 登录按钮 -->
       <div style="margin: 16px">
-        <van-button block type="primary" native-type="submit">提交</van-button>
+        <van-button block type="primary" native-type="submit">登录</van-button>
       </div>
     </van-form>
   </div>
 </template>
 
 <script>
-export default {}
+import { reactive, toRefs } from 'vue'
+// 导入用户请求API
+import { login } from '../../api/user'
+
+// 封装用户登录函数
+function userSubmit(user) {
+  const onSubmit = async () => {
+    // 调用登录请求API
+    const res = await login(user)
+    // 判断返回的内容
+    if (res.data.code === 0) {
+      console.log('用户登录成功')
+    } else {
+      console.log('用户名或密码错误')
+    }
+  }
+  return {
+    onSubmit,
+  }
+}
+
+export default {
+  setup() {
+    // 用户数据
+    const user = reactive({
+      userName: '',
+      userPwd: '',
+    })
+
+    return {
+      // 正常的方式，返回的是普通数据
+      // uers
+
+      // 解构的方式，返回响应式数据 toRefs()
+      ...toRefs(user),
+      ...userSubmit(),
+    }
+  },
+}
 </script>
 
 <style></style>
